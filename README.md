27927a8c9d65c44c687dfee7d7e7336491deffee
Martin Smith <martin@mbs3.org>
2015-10-03 20:56:17 -0500

    Initial commit of template cookbook generated by chefdk

---
+++ README.md
@@ -0,0 +1,4 @@
+# chef-provisioning-rackspace-example
+
+TODO: Enter the cookbook description here.
+
---
+++ recipes/default.rb
@@ -0,0 +1,5 @@
+#
+# Cookbook Name:: chef-provisioning-rackspace-example
+# Recipe:: default
+#
+# Copyright (c) 2015 The Authors, All Rights Reserved.
